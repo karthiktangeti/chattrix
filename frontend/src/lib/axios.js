aec026bd3c95ff0097598cff3bd4d7d0c4c88ba8
Karthik <karthiktangeti11@gmail.com>
2026-05-17 10:46:08 +0530

    changring axios in fontend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,14 @@
 import axios from "axios";
 
-export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
+const api = axios.create({
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   withCredentials: true,
 });
+
+console.log("Backend URL:", import.meta.env.VITE_API_URL);
+console.log("API URL:", api.defaults.baseURL);
+
+export default api;
 
 
 
